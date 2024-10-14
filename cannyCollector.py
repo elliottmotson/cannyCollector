8171a8de4b0d1defbe1cd115efd76d38a4956306
rtm516 <ryantmilner@hotmail.co.uk>
2024-10-14 12:53:16 +0100

    Use smtplib for emails

--- cannyCollector.py
+++ cannyCollector.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-from exchangelib import Credentials, Account, Message, DELEGATE
+from email.mime.text import MIMEText
 import json
 import sqlite3
 import os
+import traceback
+import logging
+import smtplib
+import ssl
 
+# logging.basicConfig(level=logging.DEBUG)
+
+# Use TLS 1.2 and disable ssl checking
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 class CannyCollector:
     def __init__(self):
@@ -15,39 +25,50 @@ class CannyCollector:
         self.receiver_email = os.getenv("RECEIVER_EMAIL")
         self.subject = os.getenv("EMAIL_SUBJECT")
         self.body = ""
-        self.exchange_username = os.getenv("EXCHANGE_USERNAME")
-        self.exchange_password = os.getenv("EXCHANGE_PASSWORD")
-        self.exchange_server = os.getenv("EXCHANGE_SERVER")
+        self.smtp_username = os.getenv("SMTP_USERNAME")
+        self.smtp_password = os.getenv("SMTP_PASSWORD")
+        self.smtp_server = os.getenv("SMTP_SERVER")
+        self.smtp_port = os.getenv("SMTP_PORT")
+        self.smtp_ssl = os.getenv("SMTP_SSL")
+        self.smtp_starttls = os.getenv("SMTP_STARTTLS")
+
+        self.data_folder = "./data"
+        os.makedirs(self.data_folder, exist_ok=True)
 
         self.app = Flask(__name__)
         self.app.add_url_rule('/collect', view_func=self.openCanary_webhook, methods=["POST"])
 
     def send_email(self, webhook_data):
         try:
-            # Create creds for sender account
-            credentials = Credentials(username=self.exchange_username, password=self.exchange_password)
-            account = Account(
-                primary_smtp_address=self.sender_email,
-                credentials=credentials,
-                autodiscover=False,
-                config=None,
-                access_type=DELEGATE,
-            )
+            msg = MIMEText(f"Oi!\n\nSomebodies triggered the honeypot:\n\n{json.dumps(webhook_data, indent=4)}\n\nRegards,\nCanny Collector",)
 
-            message = Message(
-                account=account,
-                subject=self.subject,
-                body=f"Oi!\n\nSomebodies triggered the honeypot:\n\n{json.dumps(webhook_data, indent=4)}\n\nRegards,\nCanny Collector",
-                to_recipients=[self.receiver_email],
-            )
-            message.send()
+            msg['Subject'] = self.subject
+            msg['From'] = self.sender_email
+            msg['To'] = self.receiver_email
+
+
+            if (self.smtp_ssl and not self.smtp_starttls):
+                s = smtplib.SMTP_SSL(self.smtp_server, self.smtp_port)
+            else:
+                s = smtplib.SMTP(self.smtp_server, self.smtp_port)
+
+            if (self.smtp_starttls):
+                s.ehlo()
+                s.starttls(context=ctx)
+                s.ehlo()
+
+            if (not self.smtp_username == '' and not self.smtp_password == ''):
+                s.login(self.smtp_username, self.smtp_password)
+
+            s.sendmail(self.sender_email, self.receiver_email, msg.as_string())
             print("Email sent successfully")
         except Exception as e:
             print("Error sending email:", e)
+            print(traceback.format_exc())
 
     def to_database(self, webhook_data):
         try:
-            conn = sqlite3.connect("alert_records.db")
+            conn = sqlite3.connect(os.path.join(self.data_folder, "alert_records.db"))
             cursor = conn.cursor()
 
             # Create table if it doesn't exist
@@ -127,7 +148,7 @@ class CannyCollector:
                 print("Parsed webhook data:", json.dumps(webhook_data, indent=4))
                 self.send_email(webhook_data)
                 self.to_database(webhook_data)
-                with open("alert_log.json", "a") as log_file:
+                with open(os.path.join(self.data_folder, "alert_log.json"), "a") as log_file:
                     json.dump(webhook_data, log_file)
                     log_file.write("\n")
                 return jsonify({"status": "success", "message": "Webhook received"}), 200
